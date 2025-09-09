57c43740e51381bfaeb5e9565f44c1fa92e8b8dd
X1ade <alex.escu137@gmail.com>
2025-09-09 16:40:17 +0100

    Initial commit

---
+++ Spotify_DJ_Requests.py
@@ -0,0 +1,78 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+import time
+import os
+import threading
+
+from dotenv import load_dotenv
+load_dotenv() # ensures enviroment variables in .env can be used, better then hardcoding secrtet values
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+track_list = []
+
+def Download_Song(track_url,song):
+    try:
+        os.system ("spotdl "+ track_url)
+        time.sleep(10)
+        print(song,' Downloaded')
+        print('Standby...')
+    except:
+        print('Song not found :(')
+
+def AddSong(track_name,track_url,artist_name):
+    track_list.append(track_url)
+    song = (track_name,artist_name)
+    print(song)
+    print(track_url)
+    return song
+
+
+class Spotify(object):
+    cid = client_id
+    secret = client_secret
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+    def Playlist_Data(playlist_link):
+        sp = Spotify.sp
+        #playlist_link = "https://open.spotify.com/playlist/05SmegprCOFAFpOo9VIAxT?si=4a2504afe4184c90"
+        playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+        track_uris = [x["track"]["uri"] 
+        for x in sp.playlist_tracks(playlist_URI)["items"]]
+        for track in sp.playlist_tracks(playlist_URI)["items"]:
+            #Track URL
+            track_url = track["track"]["external_urls"]["spotify"]
+            #Track name & artist
+            track_name = track["track"]["name"]
+            artist_name = track["track"]["artists"][0]["name"]
+        
+        check = False
+        
+        if len(track_list) > 0:
+            for i in range (0,len(track_list)):
+                if track_url == track_list[i]:
+                    check = False
+                else:
+                    check = True
+        else:
+            try:
+                song = AddSong(track_name, track_url,artist_name)
+                t = threading.Thread(target=Download_Song,args=(track_url,song,), daemon=True) 
+                t.start()      
+            except:
+                print('No tracks')
+
+        if check == True:
+            song = AddSong(track_name, track_url,artist_name)
+            t = threading.Thread(target=Download_Song,args=(track_url,song,), daemon=True)
+            t.start()   
+  
+        time.sleep(0.25)
+
+playlist_link = input('Paste playlist link here(public spotify playlist where people will add requests): ') 
+
+while True:
+   Spotify.Playlist_Data(playlist_link)
+
