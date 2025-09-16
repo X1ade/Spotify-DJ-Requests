3fc8a764526536a3134ca027d606cda7145a87d4
X1ade <alex.escu137@gmail.com>
2025-09-16 17:16:11 +0100

    Remove secrets and caches from tracking; add .gitignore

--- Spotify_DJ_Requests.py
+++ Spotify_DJ_Requests.py
@@ -4,31 +4,50 @@ from spotipy.oauth2 import SpotifyOAuth
 import time
 import os
 import threading
+import subprocess
+
 
 from dotenv import load_dotenv
-load_dotenv() # ensures enviroment variables in .env can be used, better then hardcoding secrtet values
+load_dotenv()
  
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-track_list = []
+client_id = os.getenv("SPOTIPY_CLIENT_ID")
+client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+track_list = set()
 
-def Download_Song(track_url,track_length,song):
+def Download_Song(track_url, track_length, song):
+    if track_length >= 10:
+        print("Track is too long!")
+        return
+
+    if not client_id or not client_secret:
+        print("Missing CLIENT_ID/CLIENT_SECRET in environment")
+        return
+
+    cmd = [
+        "spotdl",
+        "--client-id", client_id,
+        "--client-secret", client_secret,
+        track_url
+    ]
+
     try:
-        if track_length < 10:
-            os.system ("spotdl "+ track_url)
-            time.sleep(10)
-            print(song,' Downloaded')
-            print('Standby...')
+        print("Downloading track:", track_url)
+        ret = subprocess.run(cmd, capture_output=True, text=True)
+        if ret.returncode == 0:
+            print(song, "Downloaded")
+            print("Standby...")
         else:
-            print("Track is too long!")
-    except:
-        print('Track not found :(')
+            print("[spotdl error]", ret.stderr or ret.stdout)
+    except Exception as e:
+        print("Track not found...", e)
+
+
 
 def AddSong(track_name,track_url,artist_name):
-    track_list.append(track_url)
-    song = (track_name,artist_name)
-    print(song)
-    print(track_url)
+    track_list.add(track_url)
+    song = (f"{track_name} by {artist_name}")
+    print(song," Was found in the playlist")
+    
     return song
 
 
@@ -40,47 +59,36 @@ class Spotify(object):
 
     def Playlist_Data(playlist_link):
         sp = Spotify.sp
-        #playlist_link = "https://open.spotify.com/playlist/05SmegprCOFAFpOo9VIAxT?si=4a2504afe4184c90"
+        
         playlist_URI = playlist_link.split("/")[-1].split("?")[0]
-        track_uris = [x["track"]["uri"] 
-        for x in sp.playlist_tracks(playlist_URI)["items"]]
+
         for track in sp.playlist_tracks(playlist_URI)["items"]:
             #Track URL
             track_url = track["track"]["external_urls"]["spotify"]
             #Track name & artist
             track_name = track["track"]["name"]
             artist_name = track["track"]["artists"][0]["name"]
-
+            
             #Track duration in minutes
             track_length = track["track"]["duration_ms"] / 60000
-        
-        check = False
-        
-        if len(track_list) > 0:
-            for i in range (0,len(track_list)):
-                if track_url != track_list[i]:
-                    check = True
-                else:
-                    check = False
-                    break
-        else:
-            try:
-                song = AddSong(track_name, track_url,artist_name)
-                t = threading.Thread(target=Download_Song,args=(track_url,track_length,song,), daemon=True) 
-                t.start()      
-            except:
-                print('No tracks')
+    
+            if track_url not in track_list:
+                try:
+                    song = AddSong(track_name, track_url, artist_name)
+                    t = threading.Thread(target=Download_Song,args=(track_url,track_length,song,), daemon=True) 
+                    t.start()
+                    
+                
+                except Exception as e:
+                    print("An error has occurred:", e)
+            else:
+                continue 
 
-        if check:
-            song = AddSong(track_name, track_url,artist_name)
-            t = threading.Thread(target=Download_Song,args=(track_url,track_length,song,), daemon=True)
-            t.start()   
-  
-        
+
 playlist_link = input('Paste playlist link here(public spotify playlist where people will add requests): ') 
 
 while True:
    Spotify.Playlist_Data(playlist_link)
-   time.sleep(60)
+   time.sleep(30)
 
 
